b1900efaeb93e0d989104fbf6cda54b3004c2d32
ACEAkmoon <acealex@ukr.net>
2018-06-15 19:24:47 +0300

    2018/06/15

--- libft.h
+++ libft.h
@@ -50,7 +50,7 @@ int				ft_strnequ(char const *s1, char const *s2, size_t n);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s);
-//char			**ft_strsplit(char const *s, char c);
+char			**ft_strsplit(char const *s, char c);
 //char			*ft_itoa(int n);
 //void			ft_putchar(char c);
 //void			ft_putstr(char const *str);
